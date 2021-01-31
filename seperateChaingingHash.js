b97877498f10df4eb6a5095f44f062b3dc9dc2bc
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2021-01-31 07:17:29 -0600

    added workings

--- seperateChaingingHash.js
+++ seperateChaingingHash.js
@@ -1,5 +1,5 @@
-class HashMap {
-    constructor(initialCapacity = 9, SIZE_RATIO=3, MAX_LOAD_RATIO=0.5){
+class ChainingHashMap {
+    constructor(initialCapacity = 8, SIZE_RATIO=3, MAX_LOAD_RATIO=0.5){
         this.length = 0;
         this._hashTable = [];
         this._capacity = initialCapacity;
@@ -10,32 +10,27 @@ class HashMap {
     static _hashString(string){
         let hash = 5381;
         for(let i = 0; i < string.length; i++){
-        //Bitwise left shift with 5 0s - this would be similar to
-        //hash*31, 31 being the decent prime number
-        //but bit shifting is a faster way to do this
-        //tradeoff is understandability
             hash = (hash << 5) + hash + string.charCodeAt(i);
             hash = hash & hash;
         }
-        //making sure hash is unsigned - meaning non-negtive number.
         return hash >>> 0;
     }
 
     set(key, value){
         const loadRatio = (this.length + this._deleted +1) / this._capacity;
-        if( loadRatio > HashMap.MAX_LOAD_RATIO){
-            this._resize(this._capacity * HashMap.SIZE_RATIO);
+        if( loadRatio > ChainingHashMap.MAX_LOAD_RATIO){
+            this._resize(this._capacity * ChainingHashMap.SIZE_RATIO);
         }
         // Find the slot where this key should be in.
         const index = this._findSlot(key);
         if(!this._hashTable[index]){
             this.length++;
         }
-        this._hashTable[index] = {
+        this._hashTable[index] = [{
             key,
             value,
             DELETED: false
-        };
+        }];
     }
     get(key){
         const index = this._findSlot(key);
@@ -46,20 +41,23 @@ class HashMap {
     }
 
     _findSlot(key){
-        const hash = HashMap._hashString(key);
+        const hash = ChainingHashMap._hashString(key);
         const start = hash % this._capacity;
 
         for(let i = 0; i < start + this._capacity; i++){
             const index = i % this._capacity;
             const slot = this._hashTable[index];
+            if(!this._hashTable[index]){
+                this._hashTable[index] = [];
+            }
             if(slot === undefined || (slot.key === key &&!slot.DELETED)){
                 return index;
-            }
-        }
+            }  
     }
+}
 
     _resize(size){
-        const oldSlots = this._hashTable;
+        const oldSlots = this._hashTable.length;
         this._capacity = size;
         // Reset the length - it will get rebuilt as you add the items back
         this.length = 0;
@@ -84,4 +82,4 @@ class HashMap {
     }
 }
 
-module.exports = HashMap;
+module.exports = ChainingHashMap;
